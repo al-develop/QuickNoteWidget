3154542f7e794896c42b086829cf5909ef569122
al-develop <al.pankratz@hotmail.de>
2022-09-05 10:27:20 +0200

    some clean ups

--- QuickNoteWidget/Theme/ThemeChanger.cs
+++ QuickNoteWidget/Theme/ThemeChanger.cs
@@ -4,6 +4,12 @@ using ControlzEx.Theming;
 
 namespace QuickNoteWidget.Theme
 {
+    /// <summary>
+    /// To keep the MainWindowViewModel agnostic of the Window, 
+    /// this Helper class can be usd to change the theme from the ViewModel
+    /// Default Values are
+    ///     Accent : Cyan | Theme : Light
+    /// </summary>
     public static class ThemeChanger
     {
         public static void ChangeTheme(string accent, string theme)
